8cac53fb4c1b726d5c982d6bd7c26eaf309d3df3
charlesbarandoc <barandoccharlesmarion13@gmail.com>
2025-08-19 09:31:41 +0800

    post method added

--- src/app/hooks/useGetPosts.ts
+++ src/app/hooks/useGetPosts.ts
@@ -2,12 +2,12 @@ import { useQuery } from "@tanstack/react-query"
 import axios from "axios"
 
 const fetchPost = async () => {
-    const response = await axios.get("http://127.0.0.1:8000/api/posts")
+    const response = await axios.get("http://localhost:8000/api/posts")
     return response.data
 }
 
 const useGetPosts = () => {
     return useQuery({queryKey: ["posts"], queryFn: fetchPost},)
 }
-
+ 
 export {useGetPosts}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='flex justify-center font-bold text-6xl mt-5'>Hello World!</div>
+    <div className="bg-grey">
+        <h1 className="text-7xl font-bold text-center mt-20">Hello World!</h1>
+        <p className='mx-auto w-200 text-center mt-10'>Hello I am Charles Barandoc and currently studying at Polytechnic University of the Philippines.
+            I am taking Diploma in Computer Engineering Technology. Aiming to be good at programming. Jack of all trades and master of none.
+            I am car enthusiast a fan of JDM cars like Skyline, Supra, Civic, Nissan Zs, Lancer Evo and etc, I know how to play guitar, and I know how to cook.
+        </p>
+
+    </div>
   )
 }
 
